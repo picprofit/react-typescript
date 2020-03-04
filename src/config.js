4e8a140058d222b71658f1e2e6d4eec782f02c80
Konstantin <picprofit@gmail.com>
2020-03-04 16:21:59 +0300

    add: config with theme colors

---
+++ src/config.js
@@ -0,0 +1,12 @@
+import black from '@material-ui/core/colors/grey';
+import orange from '@material-ui/core/colors/orange';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: orange,
+    secondary: orange
+  }
+});
+
+export { theme };
