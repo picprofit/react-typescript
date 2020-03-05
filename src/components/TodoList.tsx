8237cf699da034a0aa57d7f69b6b0f27831b853d
Konstantin <picprofit@gmail.com>
2020-03-05 13:11:14 +0300

    add: delete icon

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,7 +3,9 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import taskInterface from '../interfaces/taskInterface';
 
@@ -19,16 +21,19 @@ const TodoList: React.FC<TodoListProps> = ({ tasks, deleteTask }) => {
         return (
           <ListItem
             key={item.id}
-            onClick={(e: React.MouseEvent<HTMLElement>) => {
-              e.preventDefault();
-              deleteTask(item.id);
-            }}
             button
           >
             <ListItemIcon>
               <ArrowForwardIosIcon />
             </ListItemIcon>
             <ListItemText primary={item.value} />
+            <ListItemIcon
+                onClick={(e: React.MouseEvent<HTMLElement>) => {
+                  e.preventDefault();
+                  deleteTask(item.id);
+                }}>
+              <DeleteIcon />
+            </ListItemIcon>
           </ListItem>
         );
       })}
